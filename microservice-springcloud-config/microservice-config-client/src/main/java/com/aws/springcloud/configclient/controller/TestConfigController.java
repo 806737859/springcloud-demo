04e6120f3bd6add24b9be494e756981e8be71c97
806737859 <806737859@qq.com>
2018-09-20 15:40:43 +0800

    modify

--- microservice-springcloud-config/microservice-config-client/src/main/java/com/aws/springcloud/configclient/controller/TestConfigController.java
+++ microservice-springcloud-config/microservice-config-client/src/main/java/com/aws/springcloud/configclient/controller/TestConfigController.java
@@ -1,14 +1,15 @@
 package com.aws.springcloud.configclient.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.aws.springcloud.configclient.properties.MyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestConfigController {
 
-    @Value("${myConfig}")
-    private String myConfig;
+    @Autowired
+    private MyConfig myConfig;
 
     /**
      * 配置中心不同的配置文件的myConfig不同
@@ -16,7 +17,7 @@ public class TestConfigController {
      */
     @GetMapping("/getMyConfig")
     public String getPort(){
-        return myConfig;
+        return myConfig.toString();
     }
 
 }
